8ce66aaeccd07263ea7e426563f7904cacc64a0c
Shail <shailpatel2@gmail.com>
2015-10-29 10:08:13 -0700

    Exercise 14

---
+++ ex14.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// forward_declarations: solves the problem of needing to use a function before it is defined
+void print_letters(char arg[]);
+
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
+
+    for (i = 0; i < argc; i++) {
+        print_letters(argv[i]);
+    }
+}
+
+void print_letters(char arg[])
+{
+    int i = 0;
+
+    for (i = 0; arg[i] != '\0'; i++) {
+        char ch = arg[i];
+        int can_print = isalpha(ch) || isblank(ch);        
+        if (can_print) {
+            printf("'%c' == %d ", ch, ch);
+        }
+    }
+
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) 
+{
+    print_arguments(argc, argv);
+    return 0;
+}

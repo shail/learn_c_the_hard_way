9ce1827868fd97b65a0cd00e760b8a5366803d42
Shail Patel <shailpatel2@gmail.com>
2015-12-10 14:13:53 -0800

    Commented example 17 with database

--- ex17.c
+++ ex17.c
@@ -4,9 +4,11 @@
 #include <errno.h>
 #include <string.h>
 
+// define constants through the pre-processors
 #define MAX_DATA 512
 #define MAX_ROWS 100
 
+// the name is fixed size making it less efficient but easier to store and read
 struct Address {
     int id;
     int set;
@@ -23,6 +25,10 @@ struct Connection {
     struct Database *db;
 };
 
+// kills the program with an error if anything is wrong
+// when you have an error return from a function, it will usually set an "external" variable
+// called errno to say exactly what error happened. These are just numbers so you can use perror to print the
+// message.
 void die(const char *message) {
     if (errno) {
         perror(message);
@@ -36,7 +42,7 @@ void die(const char *message) {
 void Address_print(struct Address *addr) {
     printf("%d %s %s\n", addr->id, addr->name, addr->email);
 }
-
+// rewind gets you back to the beginning of a file, you need to make sure to free errno
 void Database_load(struct Connection *conn) {
     int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
     if (rc != 1) die("Failed to load db.");
@@ -115,6 +121,9 @@ void Database_get(struct Connection *conn, int id) {
     }
 }
 
+// Copying struct prototypes -- initialize a struct on the stack and then copy into another struct in order to
+// delete the fields of a struct. Modern C allows you to simply assign one struct to another and it'll handle
+// the copying for you.
 void Database_delete(struct Connection *conn, int id) {
     struct Address addr = {.id = id, .set = 0};
     conn->db->rows[id] = addr;
@@ -141,6 +150,7 @@ int main(int argc, char *argv[]) {
     struct Connection *conn = Database_open(filename, action);
     int id = 0;
 
+    // converting string to an int
     if (argc > 3) id = atoi(argv[3]);
     if (id >= MAX_ROWS) die("There's not that many records.");
 
@@ -174,3 +184,23 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+//  Heap is all the remaining memory in your computer, and you access it with the function malloc to get more.
+//  Each time you call malloc, the OS uses internal functions to register that piece of memory to you, and
+//  then returns a pointer to it. You use free to return it to the OS so that it can be used by other
+//  programs. Otherwise you'll leak memory, but Valgrind helps you track these down.
+//
+//  The Stack is a special region of memory that stores temporary variables each function creates as locals to
+//  that function. Each argument to a function is "pushed" onto the stack, and then used inside the function.
+//  It is really a stack data strcuture, so the last thing in is the first thing out. Advantage of using a
+//  stack is that when the function exists, C compilier "pops" these variables off the stack to clean up.
+//  Three primary problems to worry about:
+//      1.) If you get a block of memory from malloc, and have the pointer on the stack, then when the
+//      function exits, the pointer will get popped off and lost.
+//      2.) If you put too much data on the stack (large structs and arrays) then you can cause a "stack
+//      overflow" and the program will abort
+//      3.) If you take a pointer to something on the stack, and then pass that or return it from your
+//      function, the function receiving it will segfault because the actual data will get popped off and
+//      disappear.
+//  This is why we have a database open and close, so we can open with creating everything and then freeing
+//  everything when we delete it.
